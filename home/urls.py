a3f9734fefbe3bac73b4421f47f603c367a4ba79
Debdipto Biswas <127871393+DebdiptoBiswas@users.noreply.github.com>
2024-11-10 23:35:53 +0530

    files

---
+++ home/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+urlpatterns = [
+    path('', views.index, name = 'home'),
+    path('about', views.about, name = 'about'),
+    path('slippers', views.slippers, name = 'slippers'),
+    path('sneakers', views.sneakers, name = 'sneakers'),
+    path('jordan', views.jordan, name = 'jordan'),
+    path('contact', views.contact, name = 'contact'),
+	path('login', views.loginUser , name = "login"),
+	path('logout', views.logoutUser , name = "logout"),
+]
